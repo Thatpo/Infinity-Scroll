37d266b82995d2f01ba3e0b68ce9ef2fc70fbfe7
Thatpo <thatos1234@gmail.com>
2021-08-11 23:46:36 -0400

    adding more styles

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = 'PASTE_YOUR_API_KEY_HERE';
+const apiKey = 'Hm4RF8KVgpU_6zrhkOEZonPdFAts7e2ROJNUSnaOwCo';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Helper Function to Set Attributes on DOM Elements
